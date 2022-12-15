73312d1bc83ccf9398c291820beef23ad9adbcab
shubhampachghare <shubhampachghare@gmail.com>
2022-12-15 19:44:42 +0530

    [Shubham]-Create Binary search tree

--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ namespace DataStructureUsingBinarySearchTree
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome in binary search tree");
+            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>(56);
+            binarySearchTree.Insert(30);
+            binarySearchTree.Insert(70);
+            binarySearchTree.Display();
         }
     }
 }
